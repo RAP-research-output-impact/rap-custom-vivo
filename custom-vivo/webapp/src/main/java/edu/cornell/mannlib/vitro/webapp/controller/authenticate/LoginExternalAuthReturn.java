ae839dabfeed7579d396aa4d94c4d9f513b0ecd0
franck <fkybus@gmail.com>
2018-06-14 00:33:33 +0200

    Trying to add custom LoginExternalAuthReturn

---
+++ custom-vivo/webapp/src/main/java/edu/cornell/mannlib/vitro/webapp/controller/authenticate/LoginExternalAuthReturn.java
@@ -0,0 +1,94 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package edu.cornell.mannlib.vitro.webapp.controller.authenticate;
+
+import static edu.cornell.mannlib.vitro.webapp.controller.authenticate.LoginExternalAuthSetup.ATTRIBUTE_REFERRER;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.cornell.mannlib.vedit.beans.LoginStatusBean.AuthenticationSource;
+import edu.cornell.mannlib.vitro.webapp.beans.UserAccount;
+import edu.cornell.mannlib.vitro.webapp.controller.accounts.user.UserAccountsFirstTimeExternalPage;
+import edu.cornell.mannlib.vitro.webapp.controller.freemarker.UrlBuilder;
+import edu.cornell.mannlib.vitro.webapp.controller.login.LoginProcessBean;
+
+/**
+ * Handle the return from the external authorization login server. If we are
+ * successful, record the login. Otherwise, display the failure.
+ */
+public class LoginExternalAuthReturn extends BaseLoginServlet {
+	private static final Log log = LogFactory
+			.getLog(LoginExternalAuthReturn.class);
+
+	/**
+	 * <pre>
+	 * Returning from the external authorization server. If we were successful,
+	 * the header will contain the name of the user who just logged in.
+	 * 
+	 * Deal with these possibilities: 
+	 * - The header name was not configured in deploy.properties. Complain.
+	 * - No username: the login failed. Complain 
+	 * - User corresponds to a User acocunt. Record the login. 
+	 * - User corresponds to an Individual (self-editor). 
+	 * - User is not recognized.
+	 * 
+	 * On entry, we expect to find:
+	 * - A LoginProcessBean, which will give us the afterLoginUrl if the login
+	 *      succeeds.
+	 * - A referrer URL, to which we will redirect if the login fails.
+	 *      TODO: is this equal to LoginProcessBean.getLoginPageUrl()?
+	 * These are removed on exit.
+	 * </pre>
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String externalAuthId = ExternalAuthHelper.getHelper(req)
+				.getExternalAuthId(req);
+		if (externalAuthId == null) {
+			log.warn("No externalAuthId.");
+			complainAndReturnToReferrer(req, resp, ATTRIBUTE_REFERRER,
+					MESSAGE_LOGIN_FAILED);
+			return;
+		}
+
+		String afterLoginUrl = LoginProcessBean.getBean(req).getAfterLoginUrl();
+		removeLoginProcessArtifacts(req);
+
+                log.warn("getting account for '" + externalAuthId + "'");
+		UserAccount userAccount = getAuthenticator(req)
+				.getAccountForExternalAuth(externalAuthId);
+		if (userAccount == null) {
+			log.warn("Creating new account for " + externalAuthId
+					+ ", return to '" + afterLoginUrl + "'");
+			UserAccountsFirstTimeExternalPage.setExternalLoginInfo(req,
+					externalAuthId, afterLoginUrl);
+			resp.sendRedirect(UrlBuilder.getUrl("/accounts/firstTimeExternal"));
+			return;
+		} else {
+			log.warn("Logging in as " + userAccount.getUri());
+			getAuthenticator(req).recordLoginAgainstUserAccount(userAccount,
+					AuthenticationSource.EXTERNAL);
+			new LoginRedirector(req, afterLoginUrl).redirectLoggedInUser(resp);
+			return;
+		}
+	}
+
+	private void removeLoginProcessArtifacts(HttpServletRequest req) {
+		req.getSession().removeAttribute(ATTRIBUTE_REFERRER);
+		LoginProcessBean.removeBean(req);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+}
