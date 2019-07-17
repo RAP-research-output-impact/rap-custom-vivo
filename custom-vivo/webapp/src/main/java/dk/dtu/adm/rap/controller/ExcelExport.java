a7c86c72336d6324cd80e0c462e32c630ef431c9
Brian Lowe <brian@ontocale.com>
2019-07-17 18:31:12 +0300

    Shoot past the hyperlink target to make it more visually obvious what's happening

--- custom-vivo/webapp/src/main/java/dk/dtu/adm/rap/controller/ExcelExport.java
+++ custom-vivo/webapp/src/main/java/dk/dtu/adm/rap/controller/ExcelExport.java
@@ -476,7 +476,7 @@ public class ExcelExport extends VitroHttpServlet {
         XSSFCell tocCell = tocRow.createCell(0);
         tocCell.setCellValue(index + ". " + tocItems.get(index - 1));
         tocCell.setCellStyle(wbs.getHyperlinkStyle());
-        int target = rowCreator.getRowIndex() + 1;
+        int target = rowCreator.getRowIndex() + 10;
         tocCell.setCellFormula
                 ("HYPERLINK(\"#" + sheet.getSheetName() + "!A" 
                         + target + "\", \"" + index + ". " 
